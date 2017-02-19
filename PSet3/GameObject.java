010e8afb79e463339cd5d8b28906589b4e5dc1ce
yakjunlun <yakjunlun@gmail.com>
2017-02-19 14:05:42 +0800

    Javadocs
    Added some documentation

--- PSet3/GameObject.java
+++ PSet3/GameObject.java
@@ -1,42 +1,89 @@
 /**
  * Created by Matthew on 17/2/2017.
  */
+
+/**
+ * <h1>Game Object</h1>
+ * This is the parent class for all objects in game.
+ * Main attributes include HP, coordinates and sprite.
+ * 
+ * @author Yak Jun Lun
+ */
 public class GameObject {
     public int HP;
     public int[] coord;
     public String sprite;
-    
+    /**
+     * Returns the HP of object.
+     * @return HP 
+     */
     public int getHP() {
         return HP;
     }
+    /**
+     * Returns the coordinates of the object.
+     * @return [x,y]
+     */
     public int[] getCoord() {
         return coord;
     }
+    /**
+     * Returns the link to the corresponding sprite.
+     * @return 
+     */
     public String getSprite(){
         return "point to sprite folder + sprite name";
     }
+    /**
+     * Used to switch to different sprites.
+     * @param newSprite 
+     */
     public void changeSprite(String newSprite) {
         sprite = newSprite;
     }
+    /**
+     * Check if the HP is less than zero. If it is, run the expire method.
+     */
     public void checkExpire() {//Check when the object's HP reaches 0
         if (HP <= 0) {
            expire();
         }
     }
+    /**
+     * Reduces HP by specified amount 
+     * @param dmg 
+     */
     public void takeDamage(int dmg) {
         HP -= dmg;
     }
+    /**
+     * Runs the script to destroy object as well as expire sprite.
+     */
     public void expire(){
         changeSprite("expired sprite path");
     }
 }
-
+/**
+ * Class for players
+ * @author Yak Jun Lun
+ */
 class Player extends GameObject {
+    /**
+     * Players username
+     */
     private String playerName; //Player's username
+    /**
+     * Class/ character that player chooses
+     */
     private String type;//Refers to Player's class, e.g. Archer, Warrior, Magician
     private int atk;
     private int orientation;
     private int range;
+    /**
+     * constructor that initialises upon gameplay
+     * @param name
+     * @param type 
+     */
     public Player(String name,String type) {//sets the different stats for each player class
         this.playerName = name;
         this.type = type;
@@ -59,6 +106,10 @@ class Player extends GameObject {
             range = 5;
         }
     }
+    /**
+     * calls method to create hitbox in front of character depending on range
+     * @return 
+     */
     public int[] attack() {
         //may vary based on type
         atkAnimation(type);
@@ -74,7 +125,10 @@ class Player extends GameObject {
             }
         }
         */
-    }
+    }/**
+     * calls animation sprite
+     * @param type 
+     */
     public void atkAnimation(String type) {
         if (type.equals("Archer")) {
             System.out.println("run Archer attack sprite");
@@ -85,12 +139,19 @@ class Player extends GameObject {
         if (type.equals("Magician")) {
             System.out.println("run Magician attack sprite");
         }
-    }
+    }/**
+     * calls sprite to get damaged
+     * and method to reduce HP
+     * @param atk 
+     */
     public void getAttacked(int atk){
         System.out.println("run damaged animation");
         takeDamage(atk);
         
-    }
+    }/**
+     * move according to button press, also changes orientation
+     * @param direction 
+     */
     public void move(String direction) {
         // need to set max limit for movement
         if (direction.equals("Left")) {
@@ -113,7 +174,11 @@ class Player extends GameObject {
     }
    
     
-    
+    /**
+     * check for range. Might be handled in game engine instead tho
+     * @param player
+     * @return 
+     */
     public boolean inRange(Player player) {//Checks if player is in range to attack another player
         if (type.equals("Archer")) {
             if (player.getCoord()[0] - coord[0] <= 3) {
