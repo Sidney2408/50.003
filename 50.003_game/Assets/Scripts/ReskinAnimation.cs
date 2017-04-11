3bfd8c730e8e7ca44e03d737739b3d5994fda4a5
Sidney2408 <sidney_suen@mymail.sutd.edu.sg>
2017-04-11 12:59:10 +0800

    Added Player color change

---
+++ 50.003_game/Assets/Scripts/ReskinAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class ReskinAnimation : MonoBehaviour {
+    public string spriteSheetName;
+
+    void LateUpdate()
+    {
+        var subSprites = Resources.LoadAll<Sprite>("Characthers/"+spriteSheetName);
+
+        foreach (var renderer in GetComponentsInChildren<SpriteRenderer>())
+        {
+            string spriteName = renderer.sprite.name;
+            Debug.Log(spriteName);
+            var newSprite = Array.Find(subSprites, item => item.name == spriteName);
+            Debug.Log(newSprite.name);
+
+            if (newSprite)
+            {
+                renderer.sprite = newSprite;
+            }
+
+        }
+
+
+
+    }
+
+}
